686ce9c25553b49e8f54f2bfcc31a7a78a7787c8
yoo.gayoung <yoo.gayoung@joins.com>
2023-01-05 16:52:45 +0900

    message

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,56 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    /*
+    @ResponseBody 를 사용
+        HTTP의 BODY에 문자 내용을 직접 반환
+        viewResolver 대신에 HttpMessageConverter 가 동작
+        기본 문자처리: StringHttpMessageConverter
+        기본 객체처리: MappingJackson2HttpMessageConverter
+        byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+     */
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,23 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * Class 설명
+ *
+ * @author yoo.gayoung
+ * @version 1.0
+ * @see == 개정이력(Modification Information) ==
+ * <p>
+ * 수정일      수정자           수정내용
+ * -------    --------    ---------------------------
+ * @since 2022-12-23
+ */
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
